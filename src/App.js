8d9c53782edaca7e6518764d08b4c6c5e2b3c6e2
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-03-13 11:20:12 -0500

    Add states for rejected and accepted data

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import React, { useEffect, useState } from 'react'
 function App() {
   const sarsData = useSARSData()
 
-  console.log(sarsData)
   return (
-    <main className="antialiased text-gray-900">
-      <section className="m-2">
+    <main className="antialiased text-gray-900 m-4">
+      <header>
         <h1 className="text-3xl">Andy you're a SARS</h1>
-        {sarsData.status === 'loading' ? (
-          <p className="text-sm text-gray-700">
-            Loading<span className="tracking-tight">. . .</span>
-          </p>
-        ) : null}
-      </section>
+        {sarsData.status === 'loading' && <Loading />}
+        {sarsData.status === 'rejected' && <Error error={sarsData.error} />}
+      </header>
+      {sarsData.status === 'resolved' && <Plot data={sarsData.data} />}
     </main>
   )
 }
@@ -31,11 +28,43 @@ const useSARSData = () => {
         setSARSData({ status: 'resolved', data, error: null })
       })
       .catch(error =>
-        setSARSData({ status: 'error', data: null, error: error })
+        setSARSData({ status: 'rejected', data: null, error: error })
       )
   }, [])
 
   return sarsData
+}
+
+function Loading() {
+  return (
+    <p className="text-sm text-gray-700 mt-2">
+      Loading<span className="tracking-tight">. . .</span>
+    </p>
+  )
+}
+
+function Error({ error }) {
+  return (
+    <div className="mt-8">
+      <h2 className="text-2xl">Oh No...</h2>
+      <details className="ml-2">
+        <summary>Expand to see the error</summary>
+        {error.message}
+        <br />
+        {error.stack}
+      </details>
+    </div>
+  )
+}
+
+function Plot({ data }) {
+  console.log(data)
+
+  return (
+    <section className="text-center">
+      <p>Success!</p>
+    </section>
+  )
 }
 
 export default App
