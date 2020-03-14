e416fe2837af72acc680c706208356dc2d811d7b
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-03-14 08:35:41 -0500

    Cleanup and modularize d3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useState, useRef, useMemo } from 'react'
-import * as d3 from 'd3'
+import React, { useEffect, useState, useRef } from 'react'
+import { select } from 'd3-selection'
+import { scaleUtc, scaleLinear, scaleOrdinal } from 'd3-scale'
+import { stack, area as d3Area } from 'd3-shape'
+import { axisBottom, axisLeft } from 'd3-axis'
 
 function App() {
   const sarsData = useSARSData()
@@ -7,7 +10,7 @@ function App() {
   return (
     <main className="antialiased text-gray-900 m-4">
       <header>
-        <h1 className="text-3xl">SARS 2003</h1>
+        <h1 className="text-3xl">SARS Data &ndash; 2003</h1>
         {sarsData.status === 'loading' && <Loading />}
         {sarsData.status === 'rejected' && <Error error={sarsData.error} />}
       </header>
@@ -71,42 +74,43 @@ const width = 900
 const height = 500
 const margin = { top: 10, right: 120, bottom: 50, left: 50 }
 const legendWidth = 30
-const color = d3
-  .scaleOrdinal()
+const color = scaleOrdinal()
   .domain(categories)
-  .range(d3.schemeCategory10)
+  .range(['#1f77b4', '#d62728', '#2ca02c'])
 
 function Plot({ data }) {
-  const barsRef = useRef()
+  const areaRef = useRef()
   const xAxisRef = useRef()
   const yAxisRef = useRef()
 
-  const aggregatedData = useAggregatedData(data)
-
   // taken from https://observablehq.com/@d3/stacked-area-chart
   useEffect(() => {
-    // Pass our data to the stack to generate the layer positions
+    const aggregatedData = getAggregatedData(data)
 
-    const series = d3.stack().keys(categories)(aggregatedData)
+    const series = stack().keys(categories)(aggregatedData)
 
-    const xScale = d3
-      .scaleUtc()
-      .domain(d3.extent(aggregatedData, d => d.date))
+    const xScaleDomain = aggregatedData.reduce(
+      ([min, max], { date }) => [Math.min(min, date), Math.max(max, date)],
+      [Infinity, -Infinity]
+    )
+    const xScale = scaleUtc()
+      .domain(xScaleDomain)
       .range([margin.left, width - margin.right])
 
-    const yScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
+    const yScaleMax = series
+      .flat()
+      .reduce((max, [, d]) => Math.max(max, d), -Infinity)
+    const yScale = scaleLinear()
+      .domain([0, yScaleMax])
       .nice()
       .range([height - margin.bottom, margin.top])
 
-    const area = d3
-      .area()
+    const area = d3Area()
       .x(d => xScale(d.data.date))
       .y0(d => yScale(d[0]))
       .y1(d => yScale(d[1]))
 
-    d3.select(barsRef.current)
+    select(areaRef.current)
       .selectAll('path')
       .data(series)
       .join('path')
@@ -115,18 +119,17 @@ function Plot({ data }) {
       .append('title')
       .text(({ key }) => key)
 
-    d3.select(xAxisRef.current)
+    select(xAxisRef.current)
       .attr('transform', `translate(0,${height - margin.bottom})`)
       .call(
-        d3
-          .axisBottom(xScale)
+        axisBottom(xScale)
           .ticks(width / 80)
           .tickSizeOuter(0)
       )
 
-    d3.select(yAxisRef.current)
+    select(yAxisRef.current)
       .attr('transform', `translate(${margin.left},0)`)
-      .call(d3.axisLeft(yScale))
+      .call(axisLeft(yScale))
       .call(g => g.select('.domain').remove())
       .call(g =>
         g
@@ -137,18 +140,17 @@ function Plot({ data }) {
           .attr('font-weight', 'bold')
           .text(aggregatedData.y)
       )
-  }, [aggregatedData])
+  }, [data])
 
   return (
-    <section className="text-center max-w-5xl max-h-full">
+    <section className="max-w-5xl max-h-full mt-8 mx-auto">
       <svg viewBox={`0, 0, ${width}, ${height}`}>
-        {categories.map((category, idx) => {
+        {[...categories].reverse().map((category, idx) => {
           const y = margin.top + 20 * idx
           const x = width - margin.right + 5
           return (
-            <g>
+            <g key={category}>
               <line
-                key={category}
                 x1={x}
                 y1={y}
                 x2={x + legendWidth}
@@ -168,7 +170,7 @@ function Plot({ data }) {
           )
         })}
 
-        <g ref={barsRef} />
+        <g ref={areaRef} />
         <g ref={xAxisRef} />
         <g ref={yAxisRef} />
       </svg>
@@ -176,31 +178,30 @@ function Plot({ data }) {
   )
 }
 
-const useAggregatedData = data =>
-  useMemo(() => {
-    let aggregationMap = new Map()
-    // go through everything and create a map of the aggregated data
-    for (const { date, cases, deaths, recoveries } of data) {
-      const values = aggregationMap.get(date)
-      const total = cases + deaths + recoveries
+const getAggregatedData = data => {
+  let aggregationMap = new Map()
+  // go through everything and create a map of the aggregated data
+  for (const { date, cases, deaths, recoveries } of data) {
+    const values = aggregationMap.get(date)
+    const total = cases + deaths + recoveries
 
-      if (values === undefined) {
-        aggregationMap.set(date, { cases, deaths, recoveries, total })
-      } else {
-        values.cases += cases
-        values.deaths += deaths
-        values.recoveries += recoveries
-        values.total += total
-      }
+    if (values === undefined) {
+      aggregationMap.set(date, { cases, deaths, recoveries, total })
+    } else {
+      values.cases += cases
+      values.deaths += deaths
+      values.recoveries += recoveries
+      values.total += total
     }
+  }
 
-    return [...aggregationMap.keys()]
-      .map(key => ({
-        // date: new Date(key),
-        date: key,
-        ...aggregationMap.get(key)
-      }))
-      .sort((a, b) => a.date - b.date)
-  }, [data])
+  return [...aggregationMap.keys()]
+    .map(key => ({
+      // date: new Date(key),
+      date: key,
+      ...aggregationMap.get(key)
+    }))
+    .sort((a, b) => a.date - b.date)
+}
 
 export default App
